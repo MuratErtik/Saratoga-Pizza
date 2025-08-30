12f8de8b51972b793f60efe95026c06e10f54e3f
Murat Ertik <dmuratertik1@gmail.com>
2025-08-30 07:57:03 -0700

    Initial commit

---
+++ src/main/java/com/example/saratogapizza/SaratogapizzaApplication.java
@@ -0,0 +1,13 @@
+package com.example.saratogapizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaratogapizzaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaratogapizzaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/saratogapizza/SaratogapizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saratogapizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaratogapizzaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
