2f3c93c3506bfccba2e015f5c6b4ec4a84973381
Murat Ertik <dmuratertik1@gmail.com>
2025-09-01 12:08:28 -0700

    Jpa Auditing enabled.

--- src/main/java/com/example/saratogapizza/SaratogapizzaApplication.java
+++ src/main/java/com/example/saratogapizza/SaratogapizzaApplication.java
@@ -2,8 +2,10 @@ package com.example.saratogapizza;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SaratogapizzaApplication {
 
     public static void main(String[] args) {
