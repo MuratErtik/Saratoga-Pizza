e85a2a996cb4c55b0f2182d58d6b56d5ecceffd9
Murat Ertik <dmuratertik1@gmail.com>
2025-09-02 02:45:16 -0700

    entities which about the details have been completed.

---
+++ src/main/java/com/example/saratogapizza/entities/BankDetails.java
@@ -0,0 +1,54 @@
+package com.example.saratogapizza.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Table(name = "bank_details")
+@EntityListeners(AuditingEntityListener.class)
+public class BankDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bankDetailsId;
+
+    @Column(nullable = false)
+    private String accountHolderName;
+
+    @Column(nullable = false)
+    private String bankName;
+
+    @Column(nullable = false)
+    private String iban;
+
+    private String accountNumber;
+    private String swiftCode;
+
+    private boolean primaryAccount = false;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+// bannkdetails and businessdetails are going to change
+    @ManyToOne
+    @JoinColumn(name = "business_id")
+    private BusinessDetails business;
+}
