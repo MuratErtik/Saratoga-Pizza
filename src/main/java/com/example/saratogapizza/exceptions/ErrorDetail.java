9859087af2e47ed9cbc2b9a0500369a2c09fc7fd
Murat Ertik <dmuratertik1@gmail.com>
2025-09-04 04:27:57 -0700

    Auth exception and Exception package have been created.

---
+++ src/main/java/com/example/saratogapizza/exceptions/ErrorDetail.java
@@ -0,0 +1,19 @@
+package com.example.saratogapizza.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorDetail {
+
+    private String error;
+
+    private String details;
+
+    private LocalDateTime timestamp;
+}
