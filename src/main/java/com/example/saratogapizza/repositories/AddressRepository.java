8ef483db9ebf8ae9b0f52edd00afb079cb242e11
Murat Ertik <dmuratertik1@gmail.com>
2025-09-05 15:55:57 -0700

    findById method has been added.

--- src/main/java/com/example/saratogapizza/repositories/AddressRepository.java
+++ src/main/java/com/example/saratogapizza/repositories/AddressRepository.java
@@ -4,7 +4,12 @@ import com.example.saratogapizza.entities.Address;
 import com.example.saratogapizza.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Set;
+
 public interface AddressRepository extends JpaRepository<Address, Long> {
 
     Address findByUserAndAddressName(User user, String addressName);
+    Set<Address> findByUser(User user);
+
+    Address findByAddressId(Long addressId);
 }
