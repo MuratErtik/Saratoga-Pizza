32d6f980d0a4c7963c577c7507eeede212678ca8
Murat Ertik <dmuratertik1@gmail.com>
2025-09-05 08:48:47 -0700

    new signup mail has been add. it has verifycode.

--- src/main/java/com/example/saratogapizza/services/EmailService.java
+++ src/main/java/com/example/saratogapizza/services/EmailService.java
@@ -36,5 +36,28 @@ public class EmailService {
         }
 
     }
+
+    public void sendVerificationEmail(String email, String code,String name,String lastname) throws MessagingException {
+
+        try {
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, "utf-8");
+            String subject = "Registration Confirmation";
+            String text = "Dear"+ name +" "+lastname+" Welcome to the Saratoga Pizza. We are so glad to see you here. " +
+                    "\nCould you please complete your register with that code ->"+ code
+
+                    +"\nHave a great day!";
+
+            messageHelper.setSubject(subject);
+            messageHelper.setText(text);
+            messageHelper.setTo(email);
+
+            javaMailSender.send(mimeMessage);
+        } catch (MailException e) {
+            throw new MailSendException("failed to send mail");
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
 
