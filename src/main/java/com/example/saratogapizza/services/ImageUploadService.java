9f798d85eec2bcebfaebf4c0ee7f0addcb20781b
Murat Ertik <dmuratertik1@gmail.com>
2025-09-17 07:18:03 -0700

    image's CRUD system has been created as a service.

---
+++ src/main/java/com/example/saratogapizza/services/ImageUploadService.java
@@ -0,0 +1,50 @@
+package com.example.saratogapizza.services;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import io.netty.util.internal.ObjectUtil;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+public class ImageUploadService {
+
+    private final Cloudinary cloudinary;
+
+    public ImageUploadService(
+            @Value("${cloudinary.cloud_name}") String cloudName,
+            @Value("${cloudinary.api_key}") String apiKey,
+            @Value("${cloudinary.api_secret}") String apiSecret) {
+
+        this.cloudinary = new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret
+        ));
+    }
+
+    public String uploadImage(MultipartFile file) throws IOException {
+        Map uploadResult = cloudinary.uploader().upload(file.getBytes(),
+                ObjectUtils.asMap("resource_type", "auto"));
+        return uploadResult.get("secure_url").toString();
+    }
+
+    public void deleteImage(String imageUrl) throws IOException {
+
+        String publicId = extractPublicIdFromUrl(imageUrl);
+        cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
+    }
+
+    private String extractPublicIdFromUrl(String url) {
+        String[] parts = url.split("/");
+        String filename = parts[parts.length - 1];
+        return filename.substring(0, filename.lastIndexOf('.'));
+    }
+}
+
+
