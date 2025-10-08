1ee36310c0d20314fade3b1d1be71e030a62c68e
Murat Ertik <dmuratertik1@gmail.com>
2025-10-08 05:34:30 -0700

    able to getting all product.

---
+++ src/main/java/com/example/saratogapizza/responses/GetAllProductResponse.java
@@ -0,0 +1,53 @@
+package com.example.saratogapizza.responses;
+
+import com.example.saratogapizza.entities.Category;
+import com.example.saratogapizza.entities.ProductSize;
+import com.example.saratogapizza.entities.ProductTopping;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+public class GetAllProductResponse {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private List<String> images;
+
+    private boolean available;
+
+    private boolean isVegetarian;
+
+    private boolean isVegan;
+
+    private int spicyLevel; // 0-5 spicy level
+
+    private Integer preparationTime; // like 30min
+
+    private Double discount; // %10.0
+
+    private Double rating; // average customer point
+
+    private String allergens; // all allergens which separate with comma,
+
+    private String tags; // examples: "New, Suggestion One"
+
+    private boolean customizable; // does  customer able to  add/remove topics
+
+    private String categoryName; // Pizza, Beverage, Desert vb.
+
+    private List<ProductSize> sizes; // Small, Mid, Big and differences of prices
+
+    private List<ProductTopping> toppings; // Extra products
+
+}
+
+
