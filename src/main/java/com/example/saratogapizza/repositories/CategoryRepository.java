12d3b62ada5347f105f8806bf4e562fac244d384
Murat Ertik <dmuratertik1@gmail.com>
2025-10-09 10:22:52 -0700

    new method has been added.

--- src/main/java/com/example/saratogapizza/repositories/CategoryRepository.java
+++ src/main/java/com/example/saratogapizza/repositories/CategoryRepository.java
@@ -10,4 +10,6 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
     Category findByCategoryId(String categoryId);
 
     List<Category> findByParentCategory(Category parent);
+
+    Category findByName(String name);
 }
