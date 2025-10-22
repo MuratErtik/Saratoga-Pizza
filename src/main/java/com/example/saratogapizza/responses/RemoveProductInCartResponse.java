d687c58f28d88b66324bd2393f18b42a9326c84e
Murat Ertik <dmuratertik1@gmail.com>
2025-10-22 05:19:37 -0700

    remove the product dto has been created.

---
+++ src/main/java/com/example/saratogapizza/responses/RemoveProductInCartResponse.java
@@ -0,0 +1,11 @@
+package com.example.saratogapizza.responses;
+
+import lombok.Data;
+
+@Data
+public class RemoveProductInCartResponse {
+
+    private String message;
+
+    private String productName;
+}
