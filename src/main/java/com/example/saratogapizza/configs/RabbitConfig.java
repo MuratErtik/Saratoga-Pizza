209e905f678a4beb1fc09996adf6de11f1ad816e
Murat Ertik <dmuratertik1@gmail.com>
2025-10-29 07:57:14 -0700

    Inventory queue has been added into RabbitMQ Config file.

--- src/main/java/com/example/saratogapizza/configs/RabbitConfig.java
+++ src/main/java/com/example/saratogapizza/configs/RabbitConfig.java
@@ -1,6 +1,7 @@
 package com.example.saratogapizza.configs;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,9 +12,23 @@ public class RabbitConfig {
     public static final String EXCHANGE = "test-exchange";
     public static final String ROUTING_KEY = "test-key";
 
+    public static final String INVENTORY_QUEUE = "inventory.queue";
+
+
     @Bean
     public Queue queue() {
         return new Queue(QUEUE, false);
+    }
+
+    @Bean
+    public Queue inventoryQueue() {
+        return new Queue(INVENTORY_QUEUE, true);
+    }
+
+    // JSON convertor (make object serialize/deserialize as automatic)
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 
     @Bean
