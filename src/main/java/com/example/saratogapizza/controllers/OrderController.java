d651f21f71c405b0d8696be78e475be1c36e7596
Murat Ertik <dmuratertik1@gmail.com>
2025-10-31 09:21:37 -0700

    order controller has been created.

--- src/main/java/com/example/saratogapizza/controllers/OrderController.java
+++ src/main/java/com/example/saratogapizza/controllers/OrderController.java
@@ -1,11 +1,15 @@
 package com.example.saratogapizza.controllers;
 
 import com.example.saratogapizza.configs.JwtUtils;
+import com.example.saratogapizza.requests.AddToCartRequest;
+import com.example.saratogapizza.requests.CreateOrderRequest;
+import com.example.saratogapizza.responses.AddProductInCartResponse;
+import com.example.saratogapizza.responses.CreateOrderResponse;
 import com.example.saratogapizza.services.CartService;
 import com.example.saratogapizza.services.OrderService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -15,6 +19,22 @@ public class OrderController {
     private final OrderService orderService;
 
     private final JwtUtils jwtUtils;
+
+    private final CartService cartService;
+
+    @PostMapping(value = "/customer/create-order")
+    public ResponseEntity<CreateOrderResponse> createOrder(
+            @RequestHeader("Authorization") String jwt,
+            @RequestBody CreateOrderRequest request
+
+    ) {
+        String token = jwt.substring(7).trim();
+        Long userId = jwtUtils.getUserIdFromToken(token);
+        System.out.println("##########userId = " + userId);
+
+        return ResponseEntity.ok(orderService.createOrder(userId,request));
+
+    }
 
 
 
